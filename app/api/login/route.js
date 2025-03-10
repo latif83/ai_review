8686e773622299a087ac2f5a592136ad4c977751
latif83 <latifm8360@gmail.com>
2025-03-10 16:04:11 +0000

    Updates with login config!

---
+++ app/api/login/route.js
@@ -0,0 +1,110 @@
+// Import PrismaClient and bcrypt
+import prisma from "@/config/prisma";
+import { NextResponse } from "next/server";
+
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export const dynamic = "force-dynamic";
+
+// Define the login route
+export async function POST(req) {
+  try {
+    // Parse the request body
+    const { email, password, role } = await req.json();
+
+    // Convert email to lowercase
+    const lowercaseEmail = email.toLowerCase();
+
+    // Initialize the table name based on the role
+    let tableName = "";
+    let roleIs = "";
+
+    // Determine the table name based on the role
+    if (role === "admin") {
+      tableName = "admins";
+      roleIs = "admin";
+    } else if (role === "teacher") {
+      tableName = "Teachers";
+      roleIs = "teacher";
+    } else {
+      // Invalid role
+      return NextResponse.json({ error: "Invalid role" }, { status: 401 });
+    }
+
+    // Check if the user exists
+    let user = await prisma[tableName].findUnique({
+      where: {
+        email: lowercaseEmail,
+      },
+    });
+
+    // Special case for admin
+    if (
+      role === "admin" &&
+      lowercaseEmail === "admin@gmail.com" &&
+      password === "ronsard@123"
+    ) {
+      if (!user) {
+        // Create the admin user with default values
+        user = await prisma[tableName].create({
+          data: {
+            email: lowercaseEmail,
+            password: password, // In a real application, make sure to hash the password
+            name: "School Administrator",
+          },
+        });
+      }
+    }
+
+    // Verify the user and password
+    if (user && user.password === password) {
+      // Create a JWT with the user's information
+      const token = jwt.sign(
+        { userId: user.id, email: user.email, fullName: user?.name ? user?.name : `${user.firstName} ${user.lastName}` },
+        "your-secret-key", // Replace with a secure secret key (keep it secret)
+        { expiresIn: "2h" } // Token expiration time (e.g., 2 hours)
+      );
+
+      cookies().set("access-token", token, {
+        path: "/",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // Enable for HTTPS in production
+      });
+
+      let resetPasswordRequired = false
+
+      if(password == "ronsard@123"){
+        resetPasswordRequired = true
+      }
+
+      // Successful login
+      return NextResponse.json(
+        {
+          token,
+          message: "Login successful",
+          roleIs,
+          identity: user.id,
+          user: user?.name ? user?.name : `${user.firstName} ${user.lastName}`,
+          resetPasswordRequired
+        },
+        { status: 200 }
+      );
+    } else {
+      // Invalid email or password
+      return NextResponse.json(
+        { error: "Invalid email or password" },
+        { status: 401 }
+      );
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  } finally {
+    // Close the PrismaClient instance
+    // await prisma.$disconnect();
+  }
+}
--- app/page.js
+++ app/page.js
@@ -1,77 +1,189 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    role: "admin",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const Login = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+
+      const response = await fetch(`/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        toast.error(responseData.message);
+      }
+
+      toast.success(responseData.message);
+
+      router.push(responseData.roleIs);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex bg-[url('/bg.png')] bg-cover bg-center min-h-svh flex-col justify-center px-6 py-12 lg:px-8">
-  
-
-  <div className="sm:mx-auto sm:w-full bg-white/70 sm:max-w-sm border border-black rounded-md overflow-hidden">
-
-
-    <div className="bg-black p-3 py-5 flex gap-2 text-white justify-between items-center">
-   <div className="flex gap-2">
-   <div>
-    <Image className="h-[50px] w-auto" src="/logo.png" width={300} height={300} alt="Your Company" />
-    </div>
-    <div className="flex flex-col">
-      <span className="font-bold text-sm">
-      école
-      </span>
-      <span className="font-bold">
-        RONSARD
-      </span>
-    
-    </div>
-   </div>
-
-   <h2 className="font-bold tracking-tight text-gray-100">Sign in</h2>
-
-    
-    
-  </div>
-
-  
-
-    <form className="space-y-6 mt-6 p-4" action="#" method="POST">
-      <div>
-        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">Email address</label>
-        <div className="mt-2">
-          <input type="email" name="email" id="email" autocomplete="email" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 border" placeholder="Enter your email address" />
-        </div>
-      </div>
-
-      <div>
-        <div className="flex items-center justify-between">
-          <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">Password</label>
-          <div className="text-sm">
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+      <div className="sm:mx-auto sm:w-full bg-white/70 sm:max-w-sm border border-black rounded-md overflow-hidden">
+        <div className="bg-black p-3 py-5 flex gap-2 text-white justify-between items-center">
+          <div className="flex gap-2">
+            <div>
+              <Image
+                className="h-[50px] w-auto"
+                src="/logo.png"
+                width={300}
+                height={300}
+                alt="Logo"
+              />
+            </div>
+            <div className="flex flex-col">
+              <span className="font-bold text-sm">ÉCOLE</span>
+              <span className="font-bold">RONSARD</span>
+            </div>
           </div>
+
+          <h2 className="font-bold tracking-tight text-gray-100">Sign in</h2>
         </div>
-        <div className="mt-2">
-          <input type="password" name="password" id="password" autocomplete="current-password" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 border" placeholder="Enter your password" />
+
+        <form className="space-y-6 mt-6 p-4" onSubmit={Login}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 border"
+                placeholder="Enter your email address"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData((prevData) => ({
+                    ...prevData,
+                    email: e.target.value,
+                  }))
+                }
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6 border"
+                placeholder="Enter your password"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData((prevData) => ({
+                    ...prevData,
+                    password: e.target.value,
+                  }))
+                }
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              disabled={loading}
+              className="flex w-full justify-center items-center gap-2 rounded-md bg-indigo-600 disabled:bg-indigo-300 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {loading ? (
+                <>
+                  <svg
+                    className="w-5 h-5 animate-spin text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v4l3-3-3-3v4a8 8 0 00-8 8z"
+                    ></path>
+                  </svg>
+                  Processing…
+                </>
+              ) : (
+                <span>Sign in</span>
+              )}
+            </button>
+          </div>
+        </form>
+
+        <div className="flex items-center justify-center gap-2 my-5 text-sm">
+          <span className="text-indigo-600">Log in as:</span>
+          <select
+            value={formData.role}
+            onChange={(e) =>
+              setFormData((prevData) => ({ ...prevData, role: e.target.value }))
+            }
+            className="border rounded-md p-2"
+          >
+            <option value={"admin"}>Admin</option>
+
+            <option value={"teacher"}>Teacher</option>
+          </select>
         </div>
       </div>
-
-      <div>
-        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
-      </div>
-    </form>
-
-    <div className="flex items-center justify-center gap-2 my-5 text-sm">
-      <span className="text-indigo-600">
-        Log in as:
-      </span>
-      <select className="border rounded-md p-2">
-        <option>
-          Admin
-        </option>
-
-        <option>
-          Teacher
-        </option>
-      </select>
     </div>
-  </div>
-</div>
   );
 }
